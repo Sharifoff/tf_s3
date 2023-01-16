4362cb8680912745f35afc0c53f23caec7b2fb2b
Sharifoff <efqanserifov@gmail.com>
2023-01-16 11:22:24 +0400

    Update provider.tf

--- MCU/provider.tf
+++ MCU/provider.tf
@@ -9,7 +9,6 @@ terraform {
 
 provider "aws" {
   region                      = var.region
-  #s3_force_path_style = true
   skip_credentials_validation = true
   skip_requesting_account_id  = true
 
